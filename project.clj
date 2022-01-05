629494bb09783e3b06bfae9ba040a1620e3efe81
gkspranger <greg@spranger.us>
2022-01-05 12:43:37 -0500

    first commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject yaaaalab "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :main ^:skip-aot yaaaalab.core)
---
+++ src/yaaaalab/core.clj
@@ -0,0 +1,59 @@
+(ns yaaaalab.core
+  (:gen-class))
+
+(def commands {"world" {:pattern #"world"
+                        :response (fn [_matches]
+                                    "world hello")}
+               "hello\\s+(\\w+)" {:pattern #"hello\s+(\w+)"
+                                  :response (fn [{:keys [message user channel]} [_match word & _rest]]
+                                              (str "you are " user
+                                                   "you said " message
+                                                   "which contains " word
+                                                   "in the channel " channel))}})
+
+(def chat1 {:message "hello world and everyone, i hope you are doing well"
+           :user "gkspranger"
+           :channel "general"
+           :metadata {}})
+
+(def chat2 {:message "this will not match on anything"
+            :user "gkspranger"
+            :channel "general"
+            :metadata {}})
+
+(defn evaluate-chat-command
+  [chat command]
+  (when command
+    (re-find (:pattern (get commands command)) (:message chat))))
+
+(defn compute-response
+  [chat command]
+  (if command
+    (let [matches (evaluate-chat-command chat command)
+          respond-with (:response (get commands command))]
+      (respond-with chat matches))
+    :default))
+
+(defn dispatch-chat
+  [chat]
+  (->> (sort (keys commands))
+       (filter #(evaluate-chat-command chat %))
+       (first)
+       (compute-response chat)
+       (assoc chat :response)))
+
+(defn -main
+  [& _args]
+  (println "hello world"))
+
+(comment
+  
+  (sort (map str (keys commands)))
+
+  (get commands "hello")
+
+  (dispatch-chat chat1)
+  (dispatch-chat chat2)
+  
+  )
+
