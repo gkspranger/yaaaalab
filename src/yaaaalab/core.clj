0fb1bd032812e2617755326f30899591bd1a2b9e
gkspranger <greg@spranger.us>
2022-01-07 16:20:15 -0500

    i think i am getting closer

--- src/yaaaalab/core.clj
+++ src/yaaaalab/core.clj
@@ -1,50 +1,63 @@
 (ns yaaaalab.core
   (:gen-class))
 
-(def commands {"world" {:pattern #"world"
-                        :response (fn [_matches]
-                                    "world hello")}
-               "hello\\s+(\\w+)" {:pattern #"hello\s+(\w+)"
-                                  :response (fn [{:keys [message user channel]} [_match word & _rest]]
-                                              (str "you are " user
-                                                   "you said " message
-                                                   "which contains " word
-                                                   "in the channel " channel))}})
+(def commands (atom{}))
+(defn sorted-command-keys [] (sort (keys @commands)))
+(defn get-command [command] (get @commands command))
 
-(def chat1 {:message "hello world and everyone, i hope you are doing well"
-           :user "gkspranger"
-           :channel "general"
-           :metadata {}})
+(defn default-response
+  [{:keys [message user]}]
+  (str "I'm sorry " user ", I don't understand your message:" \newline
+       message))
+
+(defn find-command-pattern
+  [chat command]
+  (when command
+    (re-find (:pattern (get-command command)) (:message chat))))
+
+(defn compute-response
+  [chat command]
+  (if command
+    (let [match (find-command-pattern chat command)
+          command-function (:function (get-command command))
+          command-symbol (symbol command-function)]
+      ((resolve command-symbol) (assoc chat :match match)))
+    (default-response chat)))
+
+(defn evaluate-chat
+  [chat]
+  (->> (sorted-command-keys)
+       (filter #(find-command-pattern chat %))
+       (first)
+       (compute-response chat)
+       (assoc chat :response)))
+
+(defn echo
+  {:group :echo
+   :pattern #"echo\s+(.+)"
+   :description "echo * - this will echo whatever you told it to"}
+  [{:keys [match] :as _chat}]
+  (let [[_ words] match]
+    (str "echoing " words)))
+
+(defn add-command
+  [command]
+  (let [command-meta (meta command)
+        pattern (:pattern command-meta)]
+    (swap! commands assoc (str pattern)
+           {:pattern pattern
+            :description (:description command-meta)
+            :function command})))
+
+(def chat1 {:message "echo hello"
+            :user "gkspranger"
+            :channel "general"
+            :metadata {}})
 
 (def chat2 {:message "this will not match on anything"
             :user "gkspranger"
             :channel "general"
             :metadata {}})
-
-(defn evaluate-chat-command
-  [chat command]
-  (when command
-    (re-find (:pattern (get commands command)) (:message chat))))
-
-(defn default-response
-  [{:keys [message user]}]
-  (str "i'm sorry " user ", i don't understand the message: " message))
-
-(defn compute-response
-  [chat command]
-  (if command
-    (let [matches (evaluate-chat-command chat command)
-          respond-with (:response (get commands command))]
-      (respond-with chat matches))
-    (default-response chat)))
-
-(defn dispatch-chat
-  [chat]
-  (->> (sort (keys commands))
-       (filter #(evaluate-chat-command chat %))
-       (first)
-       (compute-response chat)
-       (assoc chat :response)))
 
 (defn -main
   [& _args]
@@ -52,12 +65,13 @@
 
 (comment
   
-  (sort (map str (keys commands)))
-
-  (get commands "hello")
-
-  (dispatch-chat chat1)
-  (dispatch-chat chat2)
+  (do
+    (add-command #'echo)
+    (evaluate-chat chat1))
   
+  (do
+    (add-command #'echo)
+    (evaluate-chat chat2))
+
   )
 
