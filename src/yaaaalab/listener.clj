497e0c17537bdb897693a37dbd9e686ddef1e83e
gkspranger <greg@spranger.us>
2022-03-01 11:42:49 -0500

    more pretty

--- src/yaaaalab/listener.clj
+++ src/yaaaalab/listener.clj
@@ -4,11 +4,12 @@
                      get-namespace-mappings load-namespaces]]))
 
 (def listeners (atom []))
+
 (defn ->listeners
   []
   @listeners)
 
-(defn get-listener-namespaces
+(defn ->listener-namespaces
   []
   (filter-namespaces #".+\.listeners\..+" (all-namespaces)))
 
@@ -23,19 +24,17 @@
   (swap! listeners conj {:pattern (:pattern (meta listener))
                          :function listener}))
 
-(def get-namespace-listener-mappings (partial get-namespace-mappings
-                                              listener?))
+(def ->namespace-listener-mappings (partial get-namespace-mappings
+                                            listener?))
 
 (defn load-listeners
   []
   (reset! listeners [])
-  (let [loaded-listener-namespaces (load-namespaces (get-listener-namespaces))
-        listeners (flatten (map get-namespace-listener-mappings
+  (let [loaded-listener-namespaces (load-namespaces (->listener-namespaces))
+        listeners (flatten (map ->namespace-listener-mappings
                                 loaded-listener-namespaces))]
     (last (map load-listener listeners))))
 
 (comment
-  
-  (load-listeners)
 
-  )
+  (load-listeners))
