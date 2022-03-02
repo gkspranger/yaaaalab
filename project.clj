24c2b3dd87fb34706d9411db5c2c8132e4ca2bbd
gkspranger <greg@spranger.us>
2022-03-02 16:31:44 -0500

    make sure we go core.async

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject yaaaalab "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.5.648"]
                  [com.taoensso/timbre "5.1.2"]
                  [selmer "1.12.50"]]
   :main ^:skip-aot yaaaalab.core
