5f80aaebfba291deaedf1f385b6f3383939e494e
gkspranger <greg@spranger.us>
2022-03-23 13:58:44 -0400

    this is OK

--- src/yaaaalab/events/command.clj
+++ src/yaaaalab/events/command.clj
@@ -1,14 +1,22 @@
 (ns yaaaalab.events.command
-  (:require [taoensso.timbre :refer [debug]]))
+  (:require [taoensso.timbre :refer [debug error]]))
 
 (defn on-known-command
   {:event? true
    :id :known-command}
   [{text :text :as _message}]
-  (debug (str "command invoked: " text)))
+  (debug (str "known command invoked: " text)))
 
 (defn on-unknown-command
   {:event? true
    :id :unknown-command}
   [{text :text :as _message}]
   (debug (str "unknown command invoked: " text)))
+
+(defn on-command-exception
+  {:event? true
+   :id :command-exception}
+  [{{text :text} :message
+    exception :exception :as _command-message-exception}]
+  (error (str "command exception thrown: " text))
+  (error exception))
